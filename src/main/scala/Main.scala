1b130e95f3ff30af10906a00bfc529dbaf3ba5e9
ptuduj <paulina.tuduj@gmail.com>
2021-06-15 14:48:58 +0200

    Added graphframes lib

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,6 @@
-import org.apache.spark.graphx.lib.TriangleCount
-import org.apache.spark.graphx.{Edge, Graph}
+import org.apache.spark.graphx.Edge
 import org.apache.spark.sql.SparkSession
+import org.graphframes.GraphFrame
 
 
 object Main extends App {
@@ -9,35 +9,22 @@ object Main extends App {
     .master("local[*]")
     .getOrCreate()
 
-  val file_path = "graphDatasets/com-youtube.ungraph.csv"
+  val file_path = "graphDatasets/com-lj.ungraph.csv"
   val edges = spark.read.option("header",true).option("delimiter","\t").csv(file_path).rdd
   val edges_ = edges.map(row => Edge(row.getString(0).toLong, row.getString(1).toLong, ""))
 
-  val file_path_v = "graphDatasets/yt_target.csv"
+  val file_path_v = "graphDatasets/lj_target.csv"
   val vertices = spark.read.option("header",true).csv(file_path_v).rdd
   val vertices_ = vertices.map(row => (row.getString(0).toLong, (row.getString(1))))
 
-  //val vertices_ = vertices.map(row => (row.getString(0).toLong, (row.getString(1), row.getString(2).toLong)))
 
-  val graph = Graph(vertices_, edges_)
-
-  val startTime1 = System.currentTimeMillis
-//  val res = graph.triangleCount().vertices.collect()
-//  res.foreach(println)
-  val res = TriangleCount.runPreCanonicalized(graph).vertices.collect()
-
-  val estimatedTime1 = (System.currentTimeMillis - startTime1) / 1000.0
-  println("Time: " + estimatedTime1 + " s")
-  println(res.map(_._2).sum / 3)
-//
-//
-//  val vertice_df = spark.createDataFrame(vertices_).toDF("id", "name")
-//  val edges_df = spark.createDataFrame(edges_).toDF("src", "dst", "attr")
-//  val g = GraphFrame(vertice_df, edges_df)
-//  val startTime = System.currentTimeMillis
-//  val res = g.triangleCount.run().collect()
-//  val estimatedTime = (System.currentTimeMillis - startTime) / 1000.0
-//  println("Time: " + estimatedTime + " s")
+  val vertice_df = spark.createDataFrame(vertices_).toDF("id", "name")
+  val edges_df = spark.createDataFrame(edges_).toDF("src", "dst", "attr")
+  val g = GraphFrame(vertice_df, edges_df)
+  val startTime = System.currentTimeMillis
+  val res = g.triangleCount.run().collect()
+  val estimatedTime = (System.currentTimeMillis - startTime) / 1000.0
+  println("Time: " + estimatedTime + " s")
 
 
 }
