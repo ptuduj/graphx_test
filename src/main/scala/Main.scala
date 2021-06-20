95762604a538a58723514ab53129199c3fe770c2
ptuduj <paulina.tuduj@gmail.com>
2021-06-20 10:42:17 +0200

    added new datasets

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,4 @@
-import org.apache.spark.graphx.Edge
+import org.apache.spark.graphx.{Edge, Graph}
 import org.apache.spark.sql.SparkSession
 import org.graphframes.GraphFrame
 
@@ -9,14 +9,17 @@ object Main extends App {
     .master("local[*]")
     .getOrCreate()
 
-  val file_path = "graphDatasets/com-lj.ungraph.csv"
-  val edges = spark.read.option("header",true).option("delimiter","\t").csv(file_path).rdd
+  val file_path = "graphDatasets/musae_facebook_edges.csv"
+  val edges = spark.read.option("header",true).csv(file_path).rdd
   val edges_ = edges.map(row => Edge(row.getString(0).toLong, row.getString(1).toLong, ""))
 
-  val file_path_v = "graphDatasets/lj_target.csv"
+  val file_path_v = "graphDatasets/musae_facebook_target.csv"
   val vertices = spark.read.option("header",true).csv(file_path_v).rdd
   val vertices_ = vertices.map(row => (row.getString(0).toLong, (row.getString(1))))
 
+  //val vertices_ = vertices.map(row => (row.getString(0).toLong, (row.getString(1), row.getString(2).toLong)))
+
+  val graph = Graph(vertices_, edges_)
 
   val vertice_df = spark.createDataFrame(vertices_).toDF("id", "name")
   val edges_df = spark.createDataFrame(edges_).toDF("src", "dst", "attr")
@@ -24,7 +27,7 @@ object Main extends App {
   val startTime = System.currentTimeMillis
   val res = g.triangleCount.run().collect()
   val estimatedTime = (System.currentTimeMillis - startTime) / 1000.0
-  println("Time: " + estimatedTime + " s")
+  println("kot Time: " + estimatedTime + " s")
 
 
 }
